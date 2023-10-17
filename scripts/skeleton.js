97321375b56ac3e824522404ad7be5d4b512dd8d
LordWilliam <willjordan05@icloud.com>
2023-10-17 12:06:46 -0700

    demo07 upload

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,14 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
-    console.log($('#footerPlaceholder').load('./text/footer.html'));
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        } else {
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }
+    });
 }
 loadSkeleton();  //invoke the function
